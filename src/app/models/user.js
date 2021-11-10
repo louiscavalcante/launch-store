c81f1733df628e6c4ee08c537c4239365af9ce50
louiscavalcante <louiscavalcante@hotmail.com>
2021-11-10 07:05:31 -0400

    fixed node error

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,5 +1,4 @@
 const db = require('../../config/db.js')
-const { update } = require('../controllers/user_controller.js')
 const { encrypt } = require('../../lib/encryption_handler.js')
 
 module.exports = {
