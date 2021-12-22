f81ee4b20e16cd270f26a0a4d351a948a076cb49
louiscavalcante <louiscavalcante@hotmail.com>
2021-12-22 10:44:49 -0400

    criando base model [DONE]

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -2,75 +2,59 @@ const db = require('../../config/db.js')
 const { encryptAES } = require('../../lib/encryption_handler.js')
 const fs = require('fs')
 const Product = require('../models/product.js')
+const Base = require('../models/base.js')
+
+Base.init({ table: 'users' })
 
 module.exports = {
-	async findOne(filters) {
-		let query = 'SELECT * FROM users'
+	...Base,
+	// async create(data) {
+	// 	try {
+	// 		const query = `
+	// 		INSERT INTO users (
+	// 			name,
+	// 			email,
+	// 			password,
+	// 			cpf_cnpj,
+	// 			cep,
+	// 			address
+	// 		) VALUES ($1, $2, $3, $4, $5, $6)
+	// 		RETURNING id
+	// 	`
+	// 		const values = [
+	// 			data.name,
+	// 			data.email,
+	// 			await encryptAES(data.password),
+	// 			data.cpf_cnpj.replace(/\D/g, ''),
+	// 			data.cep.replace(/\D/g, ''),
+	// 			data.address,
+	// 		]
 
-		Object.keys(filters).map(key => {
-			// WHERE | OR | AND
-			query = `${query}
-			${key}
-			`
+	// 		const results = await db.query(query, values)
 
-			Object.keys(filters[key]).map(field => {
-				query = `${query}
-				${field} = '${filters[key][field]}'
-				`
-			})
-		})
+	// 		return results.rows[0].id
+	// 	} catch (err) {
+	// 		console.error(err)
+	// 	}
+	// },
+	// async update(id, fields) {
+	// 	let query = 'UPDATE users SET'
 
-		const results = await db.query(query)
+	// 	Object.keys(fields).map((key, index, array) => {
+	// 		if (index + 1 < array.length) {
+	// 			query = `${query}
+	// 			${key} = '${fields[key]}',`
+	// 		} else {
+	// 			// last iterator of array doesnt have -> , at the end
+	// 			query = `${query}
+	// 			${key} = '${fields[key]}'
+	// 			WHERE id = ${id}`
+	// 		}
+	// 	})
 
-		return results.rows[0]
-	},
-	async create(data) {
-		try {
-			const query = `
-			INSERT INTO users (
-				name,
-				email,
-				password,
-				cpf_cnpj,
-				cep,
-				address
-			) VALUES ($1, $2, $3, $4, $5, $6)
-			RETURNING id
-		`
-			const values = [
-				data.name,
-				data.email,
-				await encryptAES(data.password),
-				data.cpf_cnpj.replace(/\D/g, ''),
-				data.cep.replace(/\D/g, ''),
-				data.address,
-			]
-
-			const results = await db.query(query, values)
-
-			return results.rows[0].id
-		} catch (err) {
-			console.error(err)
-		}
-	},
-	async update(id, fields) {
-		let query = 'UPDATE users SET'
-
-		Object.keys(fields).map((key, index, array) => {
-			if (index + 1 < array.length) {
-				query = `${query}
-				${key} = '${fields[key]}',`
-			} else {
-				// last iterator of array doesnt have -> , at the end
-				query = `${query}
-				${key} = '${fields[key]}'
-				WHERE id = ${id}`
-			}
-		})
-
-		await db.query(query)
-		return
-	},
+	// 	await db.query(query)
+	// 	return
+	// },
 	async delete(id) {
 		let results = await db.query('SELECT * FROM products WHERE user_id = $1', [id])
 		const products = results.rows
