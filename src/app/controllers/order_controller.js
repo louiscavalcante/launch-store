c4b35b85d63fc18ea2397b78b385ac37c899b9f4
louiscavalcante <louiscavalcante@hotmail.com>
2022-01-13 21:33:24 -0400

    estilo da pagina meus pedido [start]

--- src/app/controllers/order_controller.js
+++ src/app/controllers/order_controller.js
@@ -4,6 +4,7 @@ const Order = require('../models/order.js')
 
 const mailer = require('../../lib/mailer.js')
 const Cart = require('../../lib/cart.js')
+const { formatPrice, date } = require('../../lib/utils.js')
 
 const email = (seller, product, buyer) => `
 <h2>Olá, ${seller.name}</h2>
@@ -23,6 +24,42 @@ const email = (seller, product, buyer) => `
 `
 
 module.exports = {
+	async index(req, res) {
+		let orders = await Order.findAll({ where: { buyer_id: req.session.userId } })
+
+		const getOrdersPromise = orders.map(async order => {
+			order.product = await LoadProductService.load('products', {
+				where: { id: order.product_id },
+			})
+
+			order.buyer = await User.findOne({ where: { id: order.buyer_id } })
+
+			order.seller = await User.findOne({ where: { id: order.seller_id } })
+
+			order.formattedPrice = formatPrice(order.price)
+			order.formattedTotal = formatPrice(order.total)
+
+			const statuses = {
+				open: 'Aberto',
+				sold: 'Vendido',
+				canceled: 'Cancelado',
+			}
+
+			order.formattedStatus = statuses[order.status]
+
+			const updatedAt = date(order.updated_at)
+			order.formattedUpdatedAt =
+				`${order.formattedStatus} em ${updatedAt.day}/${updatedAt.month}/${updatedAt.year} às ` +
+				`${updatedAt.hour}`.padStart(2, '0') +
+				`:${updatedAt.minutes}`.padStart(2, '0')
+
+			return order
+		})
+
+		orders = await Promise.all(getOrdersPromise)
+
+		return res.render('orders/index', { orders })
+	},
 	async post(req, res) {
 		try {
 			const cart = Cart.init(req.session.cart)
@@ -62,6 +99,9 @@ module.exports = {
 			})
 
 			await Promise.all(createOrdersPromise)
+
+			delete req.session.cart
+			Cart.init()
 
 			return res.render('orders/success')
 		} catch (err) {
--- src/routes/orders.js
+++ src/routes/orders.js
@@ -6,5 +6,6 @@ const OrderController = require('../app/controllers/order_controller.js')
 const { onlyUsers } = require('../app/middlewares/session.js')
 
 routes.post('/', onlyUsers, OrderController.post)
+routes.get('/', onlyUsers, OrderController.index)
 
 module.exports = routes
